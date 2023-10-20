6fbd839bdff3764c75a1bf776b9b854d36b0a931
FelixxDP <felix.tcdp@gmail.com>
2023-10-20 18:00:29 +0100

    now we have potential

--- SDEStarterPack.jl
+++ SDEStarterPack.jl
@@ -1,5 +1,6 @@
-using DifferentialEquations
-using Plots
+using DifferentialEquations: SDEProblem, solve
+using Plots: plot, plot!
+
 
 # SDEs of form du = f(u,p,t)dt + g(u,p,t)dW
 
@@ -17,6 +18,27 @@ function example1(dt=0.01)
 
     # ODE
     f(u, p, t) = 0
+    g(u, p, t) = 1
+
+    prob = SDEProblem(f, g, u₀, tspan)
+    sol = solve(prob, EM(), dt=dt)
+
+    plot(sol)
+end
+
+
+"""
+    example2(dt=0.01)
+
+This one has potential 🤣.
+"""
+function example2(dt=0.01)
+    # parameters
+    u₀ = 0
+    tspan = (0.0, 1.0)
+
+    # ODE
+    f(u, p, t) = -u
     g(u, p, t) = 1
 
     prob = SDEProblem(f, g, u₀, tspan)
